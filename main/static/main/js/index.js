c23ae71aff193c8aecdbf814f889f815342da3f1
FOUREX <Foxtrotserega@gmail.com>
2023-05-08 21:23:28 +0300

    Виправлена помилка при якій меню не відкривалося після першого натискання

--- main/static/main/js/index.js
+++ main/static/main/js/index.js
@@ -8,9 +8,10 @@ function seeMore(clicked_id) {
 
 function menuButton(clicked_id) {
     let element = document.getElementById("menu-content")
+    let elementStyle = getComputedStyle(element)
     let button = document.getElementById(clicked_id)
 
-    if (element.style.display === "none") {
+    if (elementStyle.display === "none") {
         element.style.display = "block"
         button.style.borderRadius = "10px 10px 0 0"
     } else {
